e0ad5ceb306306b22882cdb67970d32bd36723fe
oussama <oussamanouhar0@gmail.com>
2025-06-05 00:57:01 +0100

    Add project files.

---
+++ Responses/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ColocationAppBackend.Responses
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
