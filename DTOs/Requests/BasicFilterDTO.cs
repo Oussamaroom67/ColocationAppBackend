cabb9a3d841ba8efc5849614b64433e2dfc276e6
Maymasi <siafomaima5@gmail.com>
2025-06-10 17:11:14 +0100

    some modif

--- DTOs/Requests/BasicFilterDTO.cs
+++ DTOs/Requests/BasicFilterDTO.cs
@@ -2,5 +2,8 @@
 {
     public class BasicFilterDTO
     {
+        public string? Ville { get; set; }
+        public string? PropertyType { get; set; }
+        public int? MinPrice { get; set; }
     }
 }
